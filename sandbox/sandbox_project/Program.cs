beb72830e354daa0b92aaf065b54edf7aa961e31
hasheem-tech <hasheemnaveed19@gmail.com>
2026-02-14 22:27:15 +0500

    completed rucursion project

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -1,6 +1,7 @@
 using System;
 
 
+
 public class Program
 {
     static void Main(string[] args)
@@ -8,14 +9,34 @@ public class Program
         // This project is here for you to use as a "Sandbox" to play around
         // with any code or ideas you have that do not directly apply to
         // one of your projects.
-        var f_arr = new int[3];
-        f_arr[0] = 2;
-        f_arr[1] = 3;
-        f_arr[2] = 9;
-        // OR
-        var numbers = new[] { 1, 2, 3 };
-        // Dynmamic array is called list
-        var d_arr = new List<int> { 1, 2, 3 };
-        Console.WriteLine("Hello Sandbox World!");
+        var ways = CountWaysToClimb(5);
+        Console.WriteLine(ways);
+
+        // 1 1 1 1 1, 2 1 1 1 , 1 2 1 1, 1 1 2 1, 1 1 1 2, 2 2 1, 2 1 2, 1 2 2, 
+        // 3 1 1, 1 3 1, 1 1 3, 3 2, 2 3
+
     }
+     public static decimal CountWaysToClimb(int s)
+    {
+        // Base Cases
+        if (s < 0) 
+            return 0;
+        if (s == 0)
+            return 0;
+        if (s == 1)
+            return 1;
+        if (s == 2)
+            return 2;
+        if (s == 3)
+            return 4;
+
+        // TODO Start Problem 3
+
+        // Solve using recursion
+        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        return ways;
+    }
+
+    
 }
+
